202bed44b17c5617eb0d229a301503d0fb7e18ac
lomen-SeoJaeWan <sjw734@lomen.kr>
2024-05-28 11:33:35 +0900

    feat: test

--- src/app/page.js
+++ src/app/page.js
@@ -8,5 +8,41 @@ export default function Home() {
     console.log(res.data);
   });
 
-  return <main>테스트</main>;
+  return (
+    <main>
+      테스트
+      <div>
+        <div>
+          <h1>컨텐츠 1</h1>
+        </div>
+      </div>
+      <div>
+        <h2>컨텐츠 1</h2>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor
+          numquam laudantium nobis itaque illo ipsa, dignissimos, ab autem vel
+          animi eveniet debitis vero necessitatibus cum sed. Aliquid accusamus
+          veritatis similique.
+        </p>
+      </div>
+      <div>
+        <h2>컨텐츠 1</h2>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor
+          numquam laudantium nobis itaque illo ipsa, dignissimos, ab autem vel
+          animi eveniet debitis vero necessitatibus cum sed. Aliquid accusamus
+          veritatis similique.
+        </p>
+      </div>
+      <div>
+        <h2>컨텐츠 1</h2>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor
+          numquam laudantium nobis itaque illo ipsa, dignissimos, ab autem vel
+          animi eveniet debitis vero necessitatibus cum sed. Aliquid accusamus
+          veritatis similique.
+        </p>
+      </div>
+    </main>
+  );
 }
